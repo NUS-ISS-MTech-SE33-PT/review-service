b2fa37129a846649a336d9aa1e85549bbd918e89
Steven Yu <stevenyu@Stevens-MacBook-Air.local>
2025-09-22 18:59:07 +0800

    fix config, NLB don't support HTTP

--- terraform/main.tf
+++ terraform/main.tf
@@ -111,13 +111,13 @@ resource "aws_lb" "review_service_network_load_balancer" {
 resource "aws_lb_target_group" "review_service_target_group" {
   name        = "review-service-target-group"
   port        = 80
-  protocol    = "HTTP"
+  protocol    = "TCP"
   vpc_id      = data.terraform_remote_state.infra_vpc.outputs.aws_vpc_ecs_vpc_id
   target_type = "ip"
 
   health_check {
-    protocol            = "HTTP"
-    path                = "/health"
+    protocol            = "TCP"
+    port                = "traffic-port"
     healthy_threshold   = 2
     unhealthy_threshold = 2
     interval            = 10
@@ -128,7 +128,7 @@ resource "aws_lb_target_group" "review_service_target_group" {
 resource "aws_lb_listener" "review_service_network_load_balancer_listener" {
   load_balancer_arn = aws_lb.review_service_network_load_balancer.arn
   port              = 80
-  protocol          = "HTTP"
+  protocol          = "TCP"
 
   default_action {
     type             = "forward"
