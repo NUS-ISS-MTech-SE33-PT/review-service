4c584b2512ff65cf0e07314acd8e3b9b111cb186
Steven Yu <stevenyu@Stevens-MacBook-Air.local>
2025-10-04 13:07:43 +0800

    fix config

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -151,7 +151,6 @@ resource "aws_apigatewayv2_integration" "review_service_integration" {
   integration_uri        = aws_lb_listener.review_service_network_load_balancer_listener.arn
   connection_type        = "VPC_LINK"
   connection_id          = data.terraform_remote_state.infra_api_gateway.outputs.aws_apigatewayv2_vpc_link_ecs_vpc_link_id
-  payload_format_version = "2.0"
   integration_method     = "ANY"
 
   request_parameters = {
